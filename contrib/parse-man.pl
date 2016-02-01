b254abed9ea08a7ebdccc215a9b24d9fe0237a77
Dominique Dumont <dod@debian.org>
2016-02-01 20:48:14 +0100

    Fix errors

--- contrib/parse-man.pl
+++ contrib/parse-man.pl
@@ -2,12 +2,13 @@
 
 use strict;
 use warnings;
-use 5.10.0;
+use 5.22.0;
 
 use XML::Twig;
 use XXX;
 use Path::Tiny;
 use Config::Model::Itself;
+use experimental qw/postderef signatures/ ;
 
 my $systemd_path = path('/home/domi/debian-dev/systemd-228/man/');
 
@@ -32,39 +33,35 @@ my $rw_obj = Config::Model::Itself -> new () ;
 
 # now load the existing model to be edited
 $rw_obj -> read_all() ;
-my $meta_root = $rw_obj->config_root;
+my $meta_root = $rw_obj->meta_root;
 my $config_class;
 
-sub desc {
-    my( $t, $elt)= @_;
+my %data = ( class => [], element =>[] );
+
+sub desc ($t, $elt) {
     my $txt = $elt->trimmed_text;
-    say "before";
-    eval {
-        $meta_root->grab(step =>"class:$config_class class_dwescription", autoadd => 1)->store($txt);
-    };
-    say $@;
-    my $err = $@;
-    die "foo".$err;
-    say "after";
+    # there's black magic in XML::Twig that trash error message
+    # contained in an error object.  So the error must be stringified
+    # explicitly before being sent upward
+
+    # but it's easier to store data and handle it later outside of XML::Twig realm
+    push $data{class}->@* , [ $config_class => $txt ];
 }
 
-sub manpage {
-    my( $t, $elt)= @_;
+sub manpage ($t, $elt) {
     my $man = $elt->first_child('refentrytitle')->text;
     my $nb = $elt->first_child('manvolnum')->text;
     $elt->set_text( "L<$man($nb)>");
 }
 
-sub variable {
-    my( $t, $elt)= @_;
+sub variable  ($t, $elt) {
     my $name = $elt->first_child('term')->first_child('varname')->text;
     $name =~ s/=$//;
     my $desc = $elt->first_child('listitem')->trimmed_text;
     $desc =~ s/(\w+)=/C<$1>/g;
 
-    say "Storing $config_class $name";
-
-    #$meta_root->grab("class:$config_class element:$name description")->store($desc);
+    say "Storing class $config_class parameter $name";
+    push $data{element}->@*, [$config_class => $name => $desc ];
 }
 
 foreach my $subsystem (@list) {
@@ -73,3 +70,20 @@ foreach my $subsystem (@list) {
     $twig->parsefile($file);
 }
 
+say "Storing class descriptions";
+foreach my $cdata ($data{class}->@*) {
+    my ($config_class, $desc) = $cdata->@*;
+    my $steps = "class:$config_class class_description";
+    $meta_root->grab(step => $steps, autoadd => 1)->store($desc);
+}
+
+say "Storing class elements";
+foreach my $cdata ($data{element}->@*) {
+    my ($config_class, $element, $desc) = $cdata->@*;
+    my $steps_2_element = "class:$config_class element:$element";
+
+    $meta_root->grab(step => "$steps_2_element description", autoadd => 1)->store($desc);
+}
+
+say "Saving model";
+$rw_obj->write_all;
