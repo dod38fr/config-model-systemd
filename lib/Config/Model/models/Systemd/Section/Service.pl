0f21db62ef0459de69894076f8a4c802f72f47b8
Dominique Dumont <dod@debian.org>
2016-02-03 21:12:06 +0100

    include generated files in Service section

--- lib/Config/Model/models/Systemd/Section/Service.pl
+++ lib/Config/Model/models/Systemd/Section/Service.pl
@@ -8,6 +8,11 @@
       }
     ],
     'class_description' => 'common install section',
+    'include' => [
+      'Systemd::Common::ResourceControl',
+      'Systemd::Common::Exec',
+      'Systemd::Common::Kill'
+    ],
     'name' => 'Systemd::Section::Service'
   }
 ]
