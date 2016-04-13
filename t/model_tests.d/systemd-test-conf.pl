d6f1926d9cc15039ccdc5a66b23a28171a42eeec
Dominique Dumont <dod@debian.org>
2016-04-13 19:22:12 +0200

    test service removal

--- t/model_tests.d/systemd-test-conf.pl
+++ t/model_tests.d/systemd-test-conf.pl
@@ -19,6 +19,22 @@ $conf_dir = '/etc/systemd/system/';
         load => "service:sshd disable=1",
         wr_check => { 'service:sshd disable' => 1 },
     },
+
+    {
+        name => 'remove-service',
+        setup => {
+            'main-sshd' => $conf_dir.'sshd.service',
+            'default-sshd' => '/lib/systemd/system/sshd.service',
+            'mpd.service' => $conf_dir.'mpd.service',
+            'mpd.socket' => $conf_dir.'mpd.socket',
+        },
+        load => "service:sshd Unit Description~",
+        # file is removed because the load instruction above removes the only setting in there
+        file_check_sub => sub {
+            my $list_ref = shift ;
+            @$list_ref = grep { $_ ne '/etc/systemd/system/sshd.service'} @$list_ref;
+        }
+    },
 );
 
 1; # to keep Perl happy
