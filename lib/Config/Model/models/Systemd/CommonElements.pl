40d864045a92adf404185912de0f79c3ef4d8bba
Dominique Dumont <dod@debian.org>
2016-05-11 19:45:35 +0200

    do not write disable param in systemd file...
    default value are always written to file (i.e. '0' in this case).
    IniFile does not write a value which is identical to upstream
    default.

--- lib/Config/Model/models/Systemd/CommonElements.pl
+++ lib/Config/Model/models/Systemd/CommonElements.pl
@@ -3,12 +3,12 @@
     'element' => [
       'disable',
       {
-        'default' => '0',
         'description' => 'When true, cme will disable a configuration file supplied by the vendor by placing place a symlink to
 /dev/null with the same filename as the vendor configuration file.
 See L<systemd-system.conf> for details.',
         'summary' => 'disable configuration file supplied by the vendor',
         'type' => 'leaf',
+        'upstream_default' => '0',
         'value_type' => 'boolean'
       },
       'Unit',
