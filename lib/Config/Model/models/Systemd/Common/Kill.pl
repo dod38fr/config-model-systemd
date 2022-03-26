ddaada308aba9cae85bec0a7f80ec4995e7cd7c5
Dominique Dumont <dod@debian.org>
2022-03-26 12:12:54 +0100

    update model from Systemd 250 doc

--- lib/Config/Model/models/Systemd/Common/Kill.pl
+++ lib/Config/Model/models/Systemd/Common/Kill.pl
@@ -159,7 +159,7 @@ C<WatchdogSec>). Defaults to C<SIGABRT>.
         'value_type' => 'uniline'
       }
     ],
-    'generated_by' => 'parse-man.pl from systemd 249 doc',
+    'generated_by' => 'parse-man.pl from systemd 250 doc',
     'license' => 'LGPLv2.1+',
     'name' => 'Systemd::Common::Kill'
   }
--- lib/Config/Model/models/Systemd/Section/Timer.pl
+++ lib/Config/Model/models/Systemd/Section/Timer.pl
@@ -460,7 +460,7 @@ C<true>.',
         ]
       }
     ],
-    'generated_by' => 'parse-man.pl from systemd 249 doc',
+    'generated_by' => 'parse-man.pl from systemd 250 doc',
     'license' => 'LGPLv2.1+',
     'name' => 'Systemd::Section::Timer'
   }
