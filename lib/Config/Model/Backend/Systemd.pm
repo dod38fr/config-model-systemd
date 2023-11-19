4f383d2f228cca0414cf08b080bcfa7e448220f8
Dominique Dumont <dod@debian.org>
2023-11-19 17:47:08 +0100

    Systemd backend: remove dead code
    file deletion is also handled by Unit backend

--- lib/Config/Model/Backend/Systemd.pm
+++ lib/Config/Model/Backend/Systemd.pm
@@ -201,29 +201,6 @@ sub write ($self, %args) {
     # check      => yes|no|skip
 
     # file write is handled by Unit backend
-    return 1 if $self->instance->application =~ /file/;
-    return 1 if $self->instance->application eq 'systemd';
-
-    my $root_path = $args{root} || path('/');
-    my $dir = $args{root}->path($args{config_dir});
-    die "Unknown directory $dir" unless $dir->is_dir;
-
-    my $select_unit = $self->get_backend_arg;
-
-    # delete files for non-existing elements (deleted services)
-    foreach my $file ($dir->children($filter) ) {
-        my ($unit_type) = ($file =~ $filter);
-        my $unit_name = $file->basename($filter);
-
-        next if ($select_unit ne '*' and $unit_name !~ /$select_unit/);
-
-        my $unit_collection = $self->node->fetch_element($unit_type);
-        if (not $unit_collection->defined($unit_name)) {
-            $user_logger->warn("removing file $file of deleted service");
-            $file->remove;
-        }
-    }
-
     return 1;
 }
 
